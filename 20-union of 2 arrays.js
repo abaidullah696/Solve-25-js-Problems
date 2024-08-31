3066f1540de72a0a9f34a9ffd546880909b9a401
abaidullah696 <abaidullah696@gmail.com>
2024-08-31 18:41:17 +0500

    let and var

--- 20-union of 2 arrays.js
+++ 20-union of 2 arrays.js
@@ -8,7 +8,7 @@ function union(arr1,arr2) {
 
 console.log(union([1,2,3,4],[2,3,4,5,6]))
 
-console.log(union([5,7,3,g,5],[2,3,4,5,6]))
+console.log(union([5,7,3,3,5],[2,3,4,5,6]))
 
 
 
@@ -34,5 +34,13 @@ console.log(union1([1,2,3,4],[2,3,4,5,6]))
 
 
 
-
-
+function name() {
+    if (true) {
+        let a = 10
+        var b = 20
+    }
+    console.log(b)  
+    console.log(a)
+  
+}
+name()
